7975f7722f6db1a495f6a35a37b67a9e54befba4
Kimberly <kamueller2@gmail.com>
2019-06-18 12:53:13 -0400

    testing initClient func

--- scripts/account.js
+++ scripts/account.js
@@ -125,7 +125,6 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    // signInSuccessUrl: 'https://kamueller2.github.io/test-app',
     signInSuccessUrl: 'https://kamueller2.github.io/test-app/pages/signUp.html',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
@@ -139,56 +138,71 @@ var uiConfig = {
 ui.start('#firebaseui-auth-container', uiConfig);
 // This function will trigger when there is a login event
 firebase.auth().onAuthStateChanged(function(user) {
-    // console.log(user)
-    // Make sure there is a valid user object
-    if (user) {
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://apis.google.com/js/api.js";
+            // console.log(user)
+            // Make sure there is a valid user object
+            if (user) {
+                var script = document.createElement("script");
+                script.type = "text/javascript";
+                script.src = "https://apis.google.com/js/api.js";
 
+                function initClient() {
+                    gapi.client.init({
+                        apiKey: firebaseConfig.apiKey,
+                        discoveryDocs: firebaseConfig.discoveryDocs,
+                        clientId: firebaseConfig.clientId,
+                        scope: firebaseConfig.scopes
+                    }).then(function() {
+                        // Listen for sign-in state changes.
+                        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+                        // Handle the initial sign-in state.
+                        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+                        authorizeButton.onclick = handleAuthClick;
+                        signoutButton.onclick = handleSignoutClick;
+                    });
+                }
 
-        // Once the Google API Client is loaded, you can run your code
-        script.onload = function(e) {
-            // Initialize the Google API Client with the config object
-            gapi.client
-                .init({
-                    apiKey: firebaseConfig.apiKey,
-                    clientId: firebaseConfig.clientID,
-                    discoveryDocs: firebaseConfig.discoveryDocs,
-                    scope: firebaseConfig.scopes.join(" ")
-                })
-                // Loading is finished, so start the app
+                // Once the Google API Client is loaded, you can run your code
+                script.onload = function(e) {
+                    //     // Initialize the Google API Client with the config object
+                    //     gapi.client
+                    //         .init({
+                    //             apiKey: firebaseConfig.apiKey,
+                    //             clientId: firebaseConfig.clientID,
+                    //             discoveryDocs: firebaseConfig.discoveryDocs,
+                    //             scope: firebaseConfig.scopes.join(" ")
+                    //         })
+                    //         // Loading is finished, so start the app
+                    //         .then(function() {
+                    //             // Make sure the Google API Client is properly signed in
+                    //             if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+                    //                 startApp(user);
+                    //             } else {
+                    //                 firebase.auth().signOut(); // Something went wrong, sign out
+                    //             }
+                    //         });
+                    // };
+                    // Add to the document
+                    document.getElementsByTagName("head")[0].appendChild(script);
+                }
+            });
+
+        function startApp(user) {
+            console.log(user);
+
+            // Make sure to refresh the Auth Token in case it expires!
+            firebase.auth().currentUser.getToken()
                 .then(function() {
-                    // Make sure the Google API Client is properly signed in
-                    if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
-                        startApp(user);
-                    } else {
-                        firebase.auth().signOut(); // Something went wrong, sign out
-                    }
-                });
-        };
-        // Add to the document
-        document.getElementsByTagName("head")[0].appendChild(script);
-    }
-});
-
-function startApp(user) {
-    console.log(user);
-
-    // Make sure to refresh the Auth Token in case it expires!
-    firebase.auth().currentUser.getToken()
-        .then(function() {
-            return gapi.client.calendar.events
-                .list({
-                    calendarId: "primary",
-                    timeMin: new Date().toISOString(),
-                    showDeleted: false,
-                    singleEvents: true,
-                    maxResults: 10,
-                    orderBy: "startTime"
+                    return gapi.client.calendar.events
+                        .list({
+                            calendarId: "primary",
+                            timeMin: new Date().toISOString(),
+                            showDeleted: false,
+                            singleEvents: true,
+                            maxResults: 10,
+                            orderBy: "startTime"
+                        })
                 })
-        })
-        .then(function(response) {
-            console.log(response);
-        });
-}
+                .then(function(response) {
+                    console.log(response);
+                });
+        }
