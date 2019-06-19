f9db857298740d2c86499652321b012f4257db26
Kimberly Mueller <kamueller2@gmail.com>
2019-06-19 19:22:57 -0400

    check tokens

--- web/app.js
+++ web/app.js
@@ -14,6 +14,10 @@ const messaging = firebase.messaging();
 messaging.requestPermission()
     .then(function() {
         console.log('Have permission');
+        return messaging.getToken();
+    })
+    .then(function(token) {
+        console.log(token);
     })
     .catch(function(err) {
         console.log('Error occured');
