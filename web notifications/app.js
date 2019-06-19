6f6e64fb0fa6dc532fe5de561e990bc4d3fc3d0d
Kimberly Mueller <kamueller2@gmail.com>
2019-06-19 19:09:29 -0400

    web notifications

---
+++ web notifications/app.js
@@ -0,0 +1,20 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBN7LY1mgo8xNyiqviHoMbxTnrT4whKLXM",
+    authDomain: "project-1-b691a.firebaseapp.com",
+    databaseURL: "https://project-1-b691a.firebaseio.com",
+    projectId: "project-1-b691a",
+    storageBucket: "project-1-b691a.appspot.com",
+    messagingSenderId: "904494310957",
+    appId: "1:904494310957:web:f844b97c354d9a11"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+messaging.requestPermission()
+    .then(function() {
+        console.log('Have permission');
+    })
+    .catch(function(err) {
+        console.log('Error occured');
+    })
