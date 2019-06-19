8194783399bf2324b1052233f4967c58f7b13472
Kimberly Mueller <kamueller2@gmail.com>
2019-06-19 19:44:32 -0400

    added service worker

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBN7LY1mgo8xNyiqviHoMbxTnrT4whKLXM",
+    authDomain: "project-1-b691a.firebaseapp.com",
+    databaseURL: "https://project-1-b691a.firebaseio.com",
+    projectId: "project-1-b691a",
+    storageBucket: "project-1-b691a.appspot.com",
+    messagingSenderId: "904494310957",
+    appId: "1:904494310957:web:f844b97c354d9a11"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
